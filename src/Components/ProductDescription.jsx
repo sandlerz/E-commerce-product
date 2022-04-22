e52fed5a1fa398ab1b486d1a4d6455cd5347de16
Sebastian Mesa <sandlerzop@gmail.com>
2022-04-22 16:52:41 -0500

    project done

--- src/Components/ProductDescription.jsx
+++ src/Components/ProductDescription.jsx
@@ -1,7 +1,64 @@
-export default function ProductDescription() {
+import { useState } from "react"
+
+import minus from "../images/icon-minus.svg"
+import plus from "../images/icon-plus.svg"
+import cart from "../images/icon-cart.svg"
+
+import '../Style/productDescription.css'
+
+export default function ProductDescription({ data, handleCart }) {
+  const [count, setCount] = useState(0)
+
+  const { companyName, shoes } = data
+  const { name, price, discount, description } = shoes
+
+  const handleMinus = () => {
+    setCount(prev => prev === 0 ? 0 : prev - 1)
+  }
+
+  const handlePlus = () => {
+    setCount(prev => prev + 1)
+  }
+
+  const handleButton = () => {
+    handleCart(count)
+    setCount(0)
+  }
+
+  const sale = price * discount /100
+
   return (
-    <div>
-      PRODUCT DESCRIPTION
+    <div className="ProductDescription">
+      <h4 className="companyName">{companyName}</h4>
+      <h1 className="ProductDescription__name">{name}</h1>
+      <p className="ProductDescription__description">{description}</p>
+      <div className="price">
+        <div className="price__current">
+          <p className="current__price">
+            <span>${sale.toFixed(2)}</span>
+          </p>
+          <div className="current_discount">
+            <span>{discount}%</span>
+          </div>
+        </div>
+        <div className="price_previous">
+          <span>${price.toFixed(2)}</span>
+        </div>
+      </div>
+      <div className="btn__container">
+        <div className="counter_container">
+          <div onClick={handleMinus} className="counter">
+            <img src={minus} alt="" />
+          </div>
+          <span className="count__sum">{count}</span>
+          <div onClick={handlePlus} className="counter">
+            <img src={plus} alt="" />
+          </div>
+        </div>
+        <button className="button button--add" onClick={handleButton}>
+          <img src={cart} alt="" /> Add to cart
+        </button>
+      </div>
     </div>
   )
 }
--- src/Components/ProductImg.jsx
+++ src/Components/ProductImg.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+import { useState } from "react"
+import { useMediaQuery } from "react-responsive"
 
-import close from "../images/icon-close.svg";
-import right from "../images/next.svg";
-import left from "../images/prev.svg";
-import "../Style/ProductImg.css";
+import close from "../images/icon-close.svg"
+import right from "../images/next.svg"
+import left from "../images/prev.svg"
+import "../Style/ProductImg.css"
+import WindowOverlay from "./WindowOverlay"
 
-export default function ProductImg(props) {
-  const [imgState, setImg] = useState(0);
-  const [show, setShow] = useState(false);
+export default function ProductImg({ data }) {
+  const [imgState, setImg] = useState(0)
+  const [show, setShow] = useState(false)
 
-  const img = props.data.path[`${imgState}`];
+  const img = data.path[`${imgState}`]
 
-  const thumbnailImg = props.data.path_thumbnail.map((image, index) => (
+  const thumbnailImg = data.path_thumbnail.map((image, index) => (
     <img
       className={
-        img == props.data.path[index]
+        img == data.path[index]
           ? "img img--thumbnail img--thumbnail--selected"
           : "img img--thumbnail"
       }
@@ -22,55 +24,55 @@ export default function ProductImg(props) {
       key={index}
       onClick={() => handleFull(index)}
     />
-  ));
+  ))
 
-  const handleFull = (id) => {
-    setImg(id);
-  };
+  const handleFull = id => {
+    setImg(id)
+  }
+
+  const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 501 })
+    return isDesktop ? children : null
+  }
+
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 500 })
+    return isMobile ? children : null
+  }
 
   return (
     <div className="productImg">
-      <img
-        className="img img--big"
-        src={img}
-        alt="product"
-        onClick={() => setShow(true)}
-      />
-      <div className="thumbnail__container">{thumbnailImg}</div>
+      <Desktop>
+        <img
+          className="img img--big"
+          src={img}
+          alt="product"
+          onClick={() => setShow(true)}
+        />
+        <div className="thumbnail__container">{thumbnailImg}</div>
 
-      {/* Window Overlay */}
-      <div
-        className="newWindow"
-        style={show ? { display: "flex" } : { display: "none" }}
-      >
-        <div className="newWindow__container">
-          <img
-            src={close}
-            className="img imgClose"
-            onClick={() => setShow(false)}
-          />
-
-          <div className="imgBig__container">
-            <div
-              className="arrow--left arrow__container"
-              onClick={() => setImg((prev) => (prev === 0 ? 3 : prev - 1))}
-            >
-              <img className="arrow" src={left} alt="" />
-            </div>
-
-            <img className="img img--big" src={img} alt="product" />
-
-            <div
-              className="arrow--right arrow__container"
-              onClick={() => setImg((prev) => (prev === 3 ? 0 : prev + 1))}
-            >
-              <img className="arrow" src={right} alt="" />
-            </div>
-          </div>
-
-          <div className="thumbnail__container">{thumbnailImg}</div>
+        <WindowOverlay
+          data={data}
+          onClick={() => setShow(false)}
+          show={show}
+          handleFull={handleFull}
+        />
+      </Desktop>
+      <Mobile>
+        <div
+          className="arrow--left arrow__container"
+          onClick={() => setImg(prev => (prev === 0 ? 3 : prev - 1))}
+        >
+          <img className="arrow" src={left} alt="" />
         </div>
-      </div>
+        <img className="img img--big" src={img} alt="product" />
+        <div
+          className="arrow--right arrow__container"
+          onClick={() => setImg(prev => (prev === 3 ? 0 : prev + 1))}
+        >
+          <img className="arrow" src={right} alt="" />
+        </div>
+      </Mobile>
     </div>
-  );
+  )
 }
